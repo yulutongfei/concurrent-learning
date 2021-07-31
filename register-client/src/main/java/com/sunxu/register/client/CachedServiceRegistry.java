eeac5dc50f1398a50a193c8b5dfabfccb547e89f
孙许 <2572012446@qq.com>
2021-07-31 17:54:43 +0800

    072_案例实战:基于AtomicLong确保多线程拉取注册表版本不错乱

--- register-client/src/main/java/com/sunxu/register/client/CachedServiceRegistry.java
+++ register-client/src/main/java/com/sunxu/register/client/CachedServiceRegistry.java
@@ -2,6 +2,7 @@ package com.sunxu.register.client;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
@@ -33,6 +34,11 @@ public class CachedServiceRegistry {
      * 通信组件
      */
     private HttpSender httpSender;
+
+    /**
+     * 代表当前本地缓存注册表的版本号
+     */
+    private AtomicLong applicationVersion = new AtomicLong(0);
 
     /**
      * 构造函数
@@ -74,6 +80,25 @@ public class CachedServiceRegistry {
         return applications.getReference().getRegistry();
     }
 
+    private void fetchFullRegistry() {
+        // 一定要在发起网络请求之前,先拿到一个当时的版本号
+        Long expectedApplicationVersion = applicationVersion.get();
+        // 接着在这里发起网络请求,此时可能会有别的线程来修改这个注册表,在这个期间更新版本
+        Applications fetchedApplications = httpSender.fetchFullRegistry();
+        // 必须是发起网络请求之后,这个注册表的版本号没有人修改过,此时他才能去修改
+        // 如果在这个期间,有人修改过注册表,版本不一样了,此时就直接if不成立,不要把你拉取到
+        // 旧版本的注册表给设置进去
+        if (applicationVersion.compareAndSet(expectedApplicationVersion, expectedApplicationVersion + 1)) {
+            while (true) {
+                Applications expectedApplications = applications.getReference();
+                int expectedStamp = applications.getStamp();
+                if (applications.compareAndSet(expectedApplications, fetchedApplications, expectedStamp, expectedStamp + 1)) {
+                    break;
+                }
+            }
+        }
+    }
+
     /**
      * 负责定时拉取注册表到本地来进行缓存
      * <p>
@@ -83,15 +108,11 @@ public class CachedServiceRegistry {
 
         @Override
         public void run() {
-            // 启动拉取一次就可以了
-            Applications fetchedApplications = httpSender.fetchFullRegistry();
-            while (true) {
-                Applications expectedApplications = applications.getReference();
-                int expectedStamp = applications.getStamp();
-                if (applications.compareAndSet(expectedApplications, fetchedApplications, expectedStamp, expectedStamp + 1)) {
-                    break;
-                }
-            }
+            // 拉取全量注册表
+            // 这个操作要走网络,但是不知道为什么网络延迟,此时就是一直卡住了,数据没有返回回来
+            // 此时的这个数据就是一个旧的版本,里面仅仅包含了30个服务实例
+            // 全量拉全量注册的线程拿到数据,赋值了30个旧的服务实例
+            fetchFullRegistry();
         }
     }
 
@@ -124,14 +145,18 @@ public class CachedServiceRegistry {
                     Thread.sleep(SERVICE_REGISTRY_FETCH_INTERVAL);
 
                     // 拉取回来最近3分钟的服务实例变更
+                    // 先拉了一个增量注册表,发现和本地合并后,发现条数不对
+                    Long expectedVersion = applicationVersion.get();
                     DeltaRegistry deltaRegistry = httpSender.fetchDeltaRegistry();
-                    // 一类是注册,一类是删除
-                    // 如果是注册的话,就判断一下这个服务实例是否在这个本地缓存的注册表里
-                    // 如果不在的话,就放到本地缓存的注册表里去
-                    // 如果是删除的话,就看一下,如果服务实例存在,就给删除了
-                    // 这里其实是大量修改本地缓存的注册表,所以此处需要加锁
-                    mergeDeltaRegistry(deltaRegistry);
-                    reconcileRegistry(deltaRegistry);
+                    if (applicationVersion.compareAndSet(expectedVersion, expectedVersion + 1)) {
+                        // 一类是注册,一类是删除
+                        // 如果是注册的话,就判断一下这个服务实例是否在这个本地缓存的注册表里
+                        // 如果不在的话,就放到本地缓存的注册表里去
+                        // 如果是删除的话,就看一下,如果服务实例存在,就给删除了
+                        // 这里其实是大量修改本地缓存的注册表,所以此处需要加锁
+                        mergeDeltaRegistry(deltaRegistry);
+                        reconcileRegistry(deltaRegistry);
+                    }
 
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -182,14 +207,11 @@ public class CachedServiceRegistry {
                 // 如果数量不对,则重新全量拉取注册表进行纠正
                 // 为什么使用AtomicReference,而不是用volatile呢,此时是先读在写回,类似与i++的操作,并不是原子性的操作
                 // volatile并不保证原子性,故使用AtomicReference的CAS操作来保证操作的原子性
-                Applications fetchedApplications = httpSender.fetchFullRegistry();
-                while (true) {
-                    Applications expectedApplications = applications.getReference();
-                    int expectedStamp = applications.getStamp();
-                    if (applications.compareAndSet(expectedApplications, fetchedApplications, expectedStamp, expectedStamp + 1)) {
-                        break;
-                    }
-                }
+
+                // 重新拉取全量注册表进行纠正
+                // 进行了全量注册表最新数据的一个赋值,可能包含了40个服务实例
+                // 最新数据
+                fetchFullRegistry();
             }
         }
     }
